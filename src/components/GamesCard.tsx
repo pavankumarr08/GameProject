dca6454f538f80fe8506c334203b97dc12bcc17e
pavangit <pavanpavan0506@gmail.com>
2024-02-27 14:20:45 +0530

    Platform icons

--- src/components/GamesCard.tsx
+++ src/components/GamesCard.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import { Game } from '../hooks/useGames'
-import { Card, CardBody, Heading, Image } from '@chakra-ui/react'
+import { Card, CardBody, Heading, Image, Text } from '@chakra-ui/react'
+import PlatformIconList from './PlatformIconList';
 
 interface Props{
-    game: Game
+    game: Game;
 }
 
 const GamesCard = ({game}: Props) => {
@@ -12,9 +13,10 @@ const GamesCard = ({game}: Props) => {
         <Image src={game.background_image}/>
         <CardBody>
             <Heading fontSize='2xl'>{game.name}</Heading>
+            <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
         </CardBody>
     </Card>
   )
 }
 
-export default GamesCard
+export default GamesCard;
