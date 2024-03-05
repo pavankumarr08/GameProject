d295e9f8cf0911f35a354b62308989f07634a1f4
pavangit <pavanpavan0506@gmail.com>
2024-03-05 10:55:18 +0530

    add Emojis

--- src/components/GamesCard.tsx
+++ src/components/GamesCard.tsx
@@ -4,6 +4,7 @@ import { Card, CardBody, HStack, Heading, Image, Text } from '@chakra-ui/react'
 import PlatformIconList from './PlatformIconList';
 import CriticScore from './CriticScore';
 import getCroppedImageUrl from '../services/image-url';
+import Emoji from './Emoji';
 
 interface Props{
     game: Game;
@@ -17,7 +18,8 @@ const GamesCard = ({game}: Props) => {
             <HStack justifyContent={'space-between'} marginBottom={3}>
               <PlatformIconList platforms={game.parent_platforms.map(p => p.platform)}/>
               <CriticScore  score={game.metacritic}/>
-            <Heading fontSize='2xl'>{game.name}</Heading>
+            <Heading fontSize='2xl'>{game.name}
+            <Emoji rating={game.rating_top} /></Heading>
             </HStack>
         </CardBody>
     </Card>
